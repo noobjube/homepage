2a0cd5fcc3976d86c0cc6af39964e3eec2645bcd
noobjube <madhusudhanrajan@gmail.com>
2023-09-24 21:16:49 +0100

    Bug fix: invalid address when  accessing page using tailscale

--- homepage.py
+++ homepage.py
@@ -1,17 +1,29 @@
 from flask import Flask,render_template,redirect,url_for,request
 import os,re
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 
-@app.route("/")
+global hostname
+
+@app.route("/host",methods=["POST"])
 def deafult():
-   return ("falsk response")
+   global hostname
+   hostname = "http://"+urlparse(request.base_url).hostname+"/"
+   return redirect(hostname)
 
 @app.route("/ntfy",methods=["POST"])
 def ntfy():
    if request.method == "POST":
       text = request.form['ntfy text']
       os.system('curl -d "'+text+'" http://home.pi:8081/PiAlerts')
-   return redirect("http://home.pi")
+   return redirect(hostname)
+
+@app.route("/wakepc")
+def wakepc():
+   os.system('sudo etherwake 50:eb:f6:9f:f8:53')
+   return redirect(hostname)
+
+
 if __name__=="__main__":
     app.run(debug = True,host='0.0.0.0',port=5000)
--- static/script.js
+++ static/script.js
@@ -2,3 +2,7 @@ function pihole(){
     let hostname = location.hostname
     location.replace("http://"+hostname+"/admin")
 }
+function startpc(){
+    let hostname = location.hostname
+    location.replace("http://"+hostname+":5000/wakepc")
+}
